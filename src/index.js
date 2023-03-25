62442e7bfe240fbe55df70902c248373b4af0321
Anton-Kandaurov <kand.anton2008@gmail.com>
2023-03-25 20:50:57 +0200

    start fork js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 
+fetch(`https://restcountries.com/v3.1/all`).then
+    (res => {
+        if (!res.ok) {
+            throw new Error(res.status);
+        }
+        return res.json();
+    }).then
+    ((data) => {console.log(data)})
+ 
 
 const DEBOUNCE_DELAY = 300;
